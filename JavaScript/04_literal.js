e51825a008f18abc15d7fb67c8ee7d12916ae438
Laxman Behera <laxman.sedge@gmail.com>
2026-08-24 21:35:57 +0530

    Restore JavaScript/04_literal.js contents to fix missing output

---
+++ JavaScript/04_literal.js
@@ -0,0 +1,9 @@
+let pi =3.14;
+let age ="laxman";
+let nullvalue = null;
+let undefinedvalue;
+console.log(typeof pi);
+console.log(typeof age);
+console.log(typeof nullvalue);
+console.log(typeof undefinedvalue);
+console.log(typeof NaN);
